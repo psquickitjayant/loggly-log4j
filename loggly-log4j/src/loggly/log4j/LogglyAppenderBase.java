b490104fbb9f6f42dd7781304af5f271882d552d
Alon Kadury <kadualon@gmail.com>
2015-11-05 18:11:25 +0200

    Fix, so that custom parameters like endpointUrl would be accessible.

--- loggly-log4j/src/loggly/log4j/LogglyAppenderBase.java
+++ loggly-log4j/src/loggly/log4j/LogglyAppenderBase.java
@@ -23,7 +23,7 @@ public abstract class LogglyAppenderBase extends AppenderSkeleton {
     protected LogglyThreadPool logglyThreadPool;
 	
     
-    public LogglyAppenderBase(){
+    public void activateOptions(){
 		logglyThreadPool = new LogglyThreadPool(50);
 		
 		ensureLayout();
